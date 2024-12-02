b8d535d8deb8691af0749d2549c7d2d6948833fe
Anno Knierim <anno.knierim@tu-dortmund.de>
2024-12-02 13:36:45 +0100

    Update BaselinePlotter, still wip

--- thesis_scripts/plotting/baseline_plotter.py
+++ thesis_scripts/plotting/baseline_plotter.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 import astropy.units as un
 import cartopy.crs as ccrs
-import matplotlib.animation as anim
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -15,7 +14,7 @@ from tqdm.notebook import tqdm
 
 from pyvisgen.layouts.layouts import get_array_layout
 from pyvisgen.simulation.observation import Observation
-from pyvisgen.simulation.visibiliy import vis_loop
+from pyvisgen.simulation.visibility import vis_loop
 from thesis_scripts.baselines import plot_baselines
 
 
@@ -54,6 +53,7 @@ class BaselinePlotter:
         sensitivity_cut: float = 1e-6,
         sky_image: torch.tensor = None,
         subplots: str = "EVS",
+        start_end_only: bool = False,
     ) -> None:
         cls = cls()
         cls.array_layout = array_layout
@@ -61,7 +61,9 @@ class BaselinePlotter:
         cls.obs_start = obs_start
         cls.obs_length = obs_length
         cls.frames = frames
+        cls.sky_image = sky_image
         cls.subplots = subplots
+        cls.start_end_only = start_end_only
 
         layout = get_array_layout(array_layout, writer=True)
         cls.stations = cls._get_stations(layout)
@@ -93,6 +95,8 @@ class BaselinePlotter:
 
         cls.init_plot()
 
+        return cls
+
     def __call__(self):
         print(f"""
             Plotting
@@ -104,8 +108,8 @@ class BaselinePlotter:
             Coordinates:
                 RA:  {self.src_crd.ra:>37}
                 DEC: {self.src_crd.dec:>37}
-            Observation start: {self.obs_times[0].iso:>31}
-            Observation end: {self.obs_times[-1].iso:>33}
+            Observation start: {self.obstimes[0].iso:>31}
+            Observation end: {self.obstimes[-1].iso:>33}
             Observation length: {self.obs_length:>30}
             Number of baselines: {self.n_base:> 29}
             {50 * '-'}
@@ -130,15 +134,16 @@ class BaselinePlotter:
         elif isinstance(self.source, tuple):
             src_crd = SkyCoord(*self.source, unit="deg")
         else:
-            raise TypeError("source has to be of type str or tuple!")
+            raise TypeError("'source' has to be of type str or tuple!")
 
         return src_crd
 
-    def _num_base(self, layout):
-        return len(self.array_layout) ** 2 - len(layout)
+    def _num_base(self, stations):
+        return len(stations) ** 2 - len(stations)
 
     def _observe(self):
-        obs = Observation(self.obs_kwargs)
+        obs = Observation(**self.obs_kwargs)
+        self.num_base = obs.num_baselines * 2
 
         if (self.sky_image) and ("S" in self.subplots):
             match self.sky_image.shape[0]:
@@ -162,14 +167,19 @@ class BaselinePlotter:
 
             uu = self.vis.u
             vv = self.vis.v
-            obstimes = Time(self.vis.date, format="jd")
+            obstimes = Time(self.vis.date.cpu(), format="jd")
 
         else:
-            valid_subset = obs.baselines.get_valid_subset(obs.num_baselines, obs.device)
-            obstimes = Time(valid_subset.date, format="jd")
-
-            uu = valid_subset.u_valid
-            vv = valid_subset.v_valid
+            # valid_subset = obs.baselines.get_valid_subset(obs.num_baselines, obs.device)
+            # obstimes = Time(valid_subset.date.cpu(), format="jd")
+            #
+            # uu = valid_subset.u_valid
+            # vv = valid_subset.v_valid
+            bas = obs.baselines
+            obstimes = Time(bas.time / (60 * 60 * 24), format="mjd")
+            self.valid = bas.valid.reshape(-1, self.num_base).bool()
+            uu = bas.u
+            vv = bas.v
 
         src_crd = self.src_crd.transform_to(frame=ITRS(obstime=obstimes))
 
@@ -183,6 +193,8 @@ class BaselinePlotter:
 
     def init_plot(self, sampled_color: str = "#111e30") -> None:
         self.sampled_color = sampled_color
+        self.obstimes = self.obs.obstimes[:: self.num_base]
+        self.frame_skip = int(self.obstimes.shape[0] / self.frames)
         per_subplot_kw = {}
 
         if "E" in self.subplots:
@@ -225,12 +237,10 @@ class BaselinePlotter:
                 label="Projected source position",
             )
 
-            self.ax["E"].add_feature(
-                Nightshade(self.obs.obstimes[0].datetime, alpha=0.2)
-            )
+            self.ax["E"].add_feature(Nightshade(self.obstimes[0].datetime, alpha=0.2))
 
             self.ax["E"].set_title(
-                f"{self.obs.obstimes[0].iso} (UTC)",
+                f"{self.obstimes[0].iso} (UTC)",
                 fontfamily="monospace",
                 fontname="Fira Mono",
             )
@@ -238,28 +248,32 @@ class BaselinePlotter:
             self.ax["E"].legend(loc="upper left", bbox_to_anchor=(0, -0.02))
 
         if "V" in self.subplots:
-            u = self.obs.uu
-            v = self.obs.vv
+            self.u = self.obs.uu.cpu().reshape(-1, self.num_base)
+            self.v = self.obs.vv.cpu().reshape(-1, self.num_base)
+            self.u[~self.valid] = np.NaN
+            self.v[~self.valid] = np.NaN
 
             self.scat1 = self.ax["V"].scatter(
-                u[0],
-                v[0],
+                self.u[0],
+                self.v[0],
                 s=10,
                 c=self.sampled_color,
                 label="Sampled $(u,v)$ data",
             )
             self.scat2 = self.ax["V"].scatter(
-                u[0],
-                v[0],
+                self.u[0],
+                self.v[0],
                 s=10,
                 c="#94ab71",
                 label="Current $(u,v)$ data",
             )
-            self.scat3 = self.ax["V"].scatter(-u[0], -v[0], s=10, c=self.sampled_color)
-            self.scat4 = self.ax["V"].scatter(-u[0], -v[0], s=10, c="#94ab71")
+            self.scat3 = self.ax["V"].scatter(
+                -self.u[0], -self.v[0], s=10, c=self.sampled_color
+            )
+            self.scat4 = self.ax["V"].scatter(-self.u[0], -self.v[0], s=10, c="#94ab71")
 
-            xlim = np.abs(u.min() + 0.1 * u.min())
-            ylim = np.abs(v.min() + 0.1 * v.min())
+            xlim = np.abs(self.u.min() + 0.1 * self.u.min())
+            ylim = np.abs(self.v.min() + 0.1 * self.v.min())
 
             self.ax["V"].set(
                 xlabel=r"$u \:/\: \lambda$",
@@ -270,7 +284,7 @@ class BaselinePlotter:
 
             self.ax["V"].legend(loc="lower right", bbox_to_anchor=(1.0, 1.0))
 
-        if "S" in self.subplots:
+        if (self.sky_image) and ("S" in self.subplots):
             self.ax["S"].imshow()
 
         self.fig.suptitle(str(self.source))
@@ -311,23 +325,20 @@ class BaselinePlotter:
             self.ax["E"].legend(loc="upper left", bbox_to_anchor=(0, -0.02))
 
         if "V" in self.subplots:
-            u = self.obs.uu
-            v = self.obs.vv
-
             if i > 0:
                 self.scat1.remove()
                 self.scat3.remove()
                 self.scat1 = self.ax["V"].scatter(
-                    u[: i - 1], v[: i - 1], s=10, c=self.sampled_color
+                    self.u[: i - 1], self.v[: i - 1], s=10, c=self.sampled_color
                 )
                 self.scat3 = self.ax["V"].scatter(
-                    -u[: i - 1], -v[: i - 1], s=10, c=self.sampled_color
+                    -self.u[: i - 1], -self.v[: i - 1], s=10, c=self.sampled_color
                 )
 
             self.scat2.remove()
             self.scat4.remove()
-            self.scat2 = self.ax["V"].scatter(u[i], v[i], s=10, c="#94ab71")
-            self.scat4 = self.ax["V"].scatter(-u[i], -v[i], s=10, c="#94ab71")
+            self.scat2 = self.ax["V"].scatter(self.u[i], self.v[i], s=10, c="#94ab71")
+            self.scat4 = self.ax["V"].scatter(-self.u[i], -self.v[i], s=10, c="#94ab71")
 
     def plot(self):
         if not self.start_end_only:
